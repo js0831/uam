20e590b0ea4aea5e72259d4820e2b4c6294be968
Jener Sigua <siguajener@yahoo.com>
2020-10-26 13:48:07 +0800

    new page general role list

--- src/app/modules/general-roles/general-roles.module.ts
+++ src/app/modules/general-roles/general-roles.module.ts
@@ -4,16 +4,21 @@ import { GeneralRolesComponent } from './general-roles/general-roles.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
+import { GeneralRoleListComponent } from './general-role-list/general-role-list.component';
 
 const routes: Routes = [
   {
     path: '',
     component: GeneralRolesComponent
+  },
+  {
+    path: 'list',
+    component: GeneralRoleListComponent
   }
 ];
 
 @NgModule({
-  declarations: [GeneralRolesComponent],
+  declarations: [GeneralRolesComponent, GeneralRoleListComponent],
   imports: [
     CommonModule,
     FormsModule,
